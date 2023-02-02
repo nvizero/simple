ee95ca98347a20bb68edb6cd26a02726b5e260f9
victor20220919 <victor20220919@gmail.com>
2023-02-02 18:53:22 +0800

    golang gin server

--- main.go
+++ main.go
@@ -1,15 +1,37 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
+	"log"
+	"simple/api"
+	db "simple/db/sqlc"
+
+	_ "github.com/lib/pq"
+	"github.com/techschool/simplebank/util"
 )
 
-//var testQueries *db.Queries
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:root@localhost:5432/simple?sslmode=disable"
+	serverAddress = "0.0.0.0:8081"
+)
 
 func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
 
-	fmt.Println("qwe")
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
 
-	//account, _ := testQueries.CreateAccounts(context.Background(), arg)
-	//fmt.Prinlnt(account)
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start("0.0.0.0:8081")
+	if err != nil {
+		log.Fatal("cannot start server :", err)
+	}
 }
