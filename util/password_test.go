e16410bf6c3c4d7a29eb3f3f18a84abd186844e9
victor20220919 <victor20220919@gmail.com>
2023-02-15 13:59:49 +0800

    fix password and randomstring

--- util/password_test.go
+++ util/password_test.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -10,5 +11,7 @@ func TestPassword(t *testing.T) {
 	password := RandomString(6)
 	hashPassword, err := HashPassword(password)
 	require.NoError(t, err)
+	fmt.Println(password)
 	require.NotEmpty(t, hashPassword)
+	require.Equal(t, len(password), 6)
 }
